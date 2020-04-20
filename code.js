110da7efee5b5305cbf7835417ee785026f5b726
Alexey October <ALEXEY_OCTOBER@protonmail.com>
2020-04-21 02:19:16 +0300

    Добавление базовой структуры

---
+++ code.js
@@ -0,0 +1,56 @@
+// Переменные для нахождения корабля, 3 шт
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+// Переменные для действий
+var guess;
+var hits = 0;
+var guesses = 0;
+
+// Переменная статуса
+var isSunk = false;
+
+// Через цикл while пока переменная false
+while (isSunk === false)
+{
+    // ******* 1
+    // Запрос данных
+    guess = prompt("Ready, aim, fire! (Enter number 1 - 6)");
+    // Проверка на правильность диапозона чисел
+    if (guess < 0 || guess > 6) 
+    {
+        alert("Please enter a valid cell number");
+    }
+    // Добавление + 1 к количеству попыток
+    else
+    {
+        guess + 1;
+    }
+
+    // ******* 2
+    // Проверка совпадения локации корабля и выстрела
+    if (guess == location1 || guess == location2 || guess == location3 )
+    {
+        hits = hits + 1;
+        alert("Hit.");
+    }
+    else
+    {
+        alert("Miss.");
+    }
+
+    // ******* 3
+    // Проверка количества попаданий на равенство трем
+    if (hits == 3)
+    {
+        isSunk = true;
+        alert("You sank Battleship");
+    } 
+}
+
+// ******* 4
+// После завершения цикла и окончаняи игры
+var stats = "Your took " + guesses + " to sink Battleship. " + "Your Accuracy was " + (3 / guesses);
+
+alert(stats);
